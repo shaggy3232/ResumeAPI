4b02c2d264feb8eb3f1d36e7e4472e4e63f70d00
Shaggy Bremnath <43152563+shaggy3232@users.noreply.github.com>
2024-12-27 22:14:51 -0500

    push some wokr

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shaggy3232/ResumeAPI
 
 go 1.22.3
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,140 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("what good folks")
+	//initialize router
+
+	//start server
+
+}
+
+//setup up router function
+//initalize a new chi router
+//set up the middle wares
+//Logger
+//Recoverer
+//cors
+// set cors option for origin
+// set cors option for the methods
+
+// registor routes
+
+//create a function to registor all of the routes
+
+//create routes for each of the end points
+//assign method to handler functions
+
+//create handler for each endpoint
+//create the CRUD function for each of the endpoints
+
+///Contact
+
+var contactFilePath = filepath.Join("data", "contact.json")
+
+func UpdateContact(w http.ResponseWriter, r *http.Request) {
+
+	var contact Contact
+
+	if err := ReadJson(contactFilePath, &contact); err != nil {
+		http.Error(w, "Error Reading contact from contact.json", http.StatusInternalServerError)
+		return
+	}
+
+	var newContact Contact
+
+	if err := json.NewDecoder(r.Body).Decode(&newContact); err != nil {
+		http.Error(w, "Error decoding Contact from request", http.StatusBadRequest)
+		return
+	}
+	contact = newContact
+
+	if err := WriteJson(contactFilePath, &contact); err != nil {
+		http.Error(w, "Error writing new contact to contact.json", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(newContact)
+	return
+}
+
+func GetContacts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+///Jobs
+
+///Projects
+
+//Util function to Read and write Json
+
+func ReadJson(filepath string, target interface{}) error {
+	file, err := os.open(filepath)
+	if err != nil {
+		return err
+	}
+
+	defer file.close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, target)
+}
+
+func WriteJson(filepath string, data interface{}) error {
+	file, err := os.open(filepath)
+	if err != nil {
+		return err
+	}
+
+	defer file.close()
+
+	jsonData, err := json.MarshalIndent(data, "", " ")
+
+	if err != nil {
+		return err
+	}
+
+	_, err := file.write(file, jsonData)
+
+}
+
+//create the models for the each sections of the resumes
+
+type Resume struct {
+	Contact  Contact
+	Jobs     []Jobs
+	Projects []Projects
+	Skills   []string
+}
+
+type Contact struct {
+	FirstName string
+	LastName  string
+	Email     string
+	Github    string
+}
+
+type Jobs struct {
+	Company      string
+	Duration     string
+	Task         []string
+	Title        string
+	Technologies []string
+}
+
+type Projects struct {
+	Name        string
+	Description string
+	Stack       string
+	Purpose     string
+	Github      string
 }
